68dcdaf209e89a16d405927a726e84378d81393d
KingBbwe <banamunyati@gmail.com>
2024-12-06 19:24:00 +0200

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,1 +1,16 @@
+import React from 'react';
+import PlayerDashboard from './components/PlayerDashboard';
+import GMPortal from './components/GMPortal';
+
+function App() {
+    return (
+        <div>
+            <h1>Welcome to The Crystal Bank</h1>
+            <PlayerDashboard />
+            <GMPortal />
+        </div>
+    );
+}
+
+export default App;
 
