2338d40943a3f39ea3c082685e24ec063d11989e
KingBbwe <banamunyati@gmail.com>
2024-12-16 03:10:57 +0200

    Delete web3connection directory

--- web3connection/testAgent.js
+++
@@ -1,7 +0,0 @@
-const { HttpAgent } = require('@dfinity/agent');
-const { TextEncoder } = require('util');
-
-global.TextEncoder = TextEncoder;
-
-const agent = new HttpAgent({ host: 'http://localhost:8000' });
-console.log('Agent created:', agent);
