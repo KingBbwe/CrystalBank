b8621665ffb9569a6a576f768acaeecdd3e28b33
KingBbwe <banamunyati@gmail.com>
2024-12-18 04:00:27 +0200

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,12 +2,14 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+    root: "src", // Specify 'src' as the root directory
     plugins: [react()],
     server: {
         port: 3000,
     },
     build: {
-        outDir: "build",
+        outDir: "../build", // Adjust path since 'root' is now 'src'
     },
-    base: "./",
+    base: "./", // Ensures relative paths for assets
 });
+
