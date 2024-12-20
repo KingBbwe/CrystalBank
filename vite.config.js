9f93803aa308031d6a3f1a1a9193e83631e81c7b
KingBbwe <banamunyati@gmail.com>
2024-12-20 17:51:23 +0200

    Create vite.config.js

---
+++ vite.config.js
@@ -0,0 +1,15 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+export default defineConfig({
+    root: "src", // Define 'src' as the root directory
+    plugins: [react()],
+    server: {
+        port: 3000, // Local development server port
+    },
+    build: {
+        outDir: "../build", // Output directory in the project root
+        emptyOutDir: true,  // Ensures 'build' folder is cleaned before each build
+    },
+    base: "./", // Ensures relative paths for assets
+});
